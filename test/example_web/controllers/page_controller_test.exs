f348c7e662c8b388330167feee39d854053210d6
Nathan Herald <me@nathanherald.com>
2018-10-11 14:06:20 -0700

    Fix broken test

--- test/example_web/controllers/page_controller_test.exs
+++ test/example_web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule ExampleWeb.PageControllerTest do
   use ExampleWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "<div id=\"app\"></div>"
   end
 end
